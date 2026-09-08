cafe6eaaa5e8f7f155731712b5267e84ce306052
Bello Olamilekan <belloolamilekan661@gmail.com>
2026-09-08 18:41:42 +0100

    switched to list of enermies and refactored the battle flow.

--- Program.cs
+++ Program.cs
@@ -5,59 +5,82 @@ class Program
     static void Main(string[] args)
     {
         Player hero = new Player(name: "Aria", health: 100, level: 1, attackPower: 20, maxHealth: 100, potions: 3);
-        Enemy enemy = new Enemy(name: "Goblin", health: 40, maxHealth: 40, attackPower: 5);
+        List<Enemy> enemies = new List<Enemy>();
+        enemies.Add(new Enemy(name: "Goblin", health: 40, maxHealth: 40, attackPower: 50));
+        enemies.Add(new Enemy(name: "Orc", health: 50, maxHealth: 50, attackPower: 7));
+        enemies.Add(new Enemy(name: "Gollum", health: 20, maxHealth: 20, attackPower: 2));
+        
+        RunBattle(hero:hero, enemies: enemies);
+
+
+    }
+
+    static void RunBattle(Player hero, List<Enemy> enemies)
+    {
         bool isValidChoice;
 
-
-
-        while (hero.IsAlive && enemy.IsAlive)
+        foreach (Enemy enemy in enemies)
         {
-            Console.WriteLine($"{hero.CharacterName}: {hero.CurrentHealth}/{hero.MaxHealthValue} HP, {hero.AvailablePotions} {(hero.AvailablePotions > 1 ? "potions" : "potion")} left");
-            Console.WriteLine($"Choose an action: attack, heal, potion: "); 
-            string input = Console.ReadLine();
+            Console.WriteLine($"A wild {enemy.CharacterName} appears!");
             
-            switch (input)
+            while (hero.IsAlive && enemy.IsAlive)
             {
-                case "attack":
-                    Console.WriteLine($"You chose to attack.");
-                    hero.Attack(target: enemy);
+                Console.WriteLine($"{hero.CharacterName}: {hero.CurrentHealth}/{hero.MaxHealthValue} HP, {hero.AvailablePotions} {(hero.AvailablePotions > 1 ? "potions" : "potion")} left");
+                Console.WriteLine($"Choose an action: attack, heal, potion: "); 
+                string input = Console.ReadLine();
+                bool success = Enum.TryParse(input, true, out PlayerAction action);
+                if (success)
+                {
+                    switch (action)
+                    {
+                        case PlayerAction.Attack:
+                            Console.WriteLine($"You chose to attack.");
+                            hero.Attack(target: enemy);
+                            break;
+                        case PlayerAction.Heal:
+                            Console.WriteLine($"You chose to heal.");
+                            hero.Heal(amount:10);
+                            break;
+                        case PlayerAction.Potion:
+                            Console.WriteLine($"You chose to use a potion");
+                            hero.UsePotion();
+                            break;
+                    }
                     isValidChoice = true;
-                    break;
-                case "heal":
-                    Console.WriteLine($"You chose to heal.");
-                    hero.Heal(amount:10);
-                    isValidChoice = true;
-                    break;
-                case "potion":
-                    Console.WriteLine($"You chose to use a potion");
-                    hero.UsePotion();
-                    isValidChoice = true;
-                    break;
-                default:
+                }
+                else
+                {
                     Console.WriteLine($"Not a valid choice");
                     isValidChoice = false;
-                    break;
-            }
 
-            if (isValidChoice)
-            {
-                enemy.Attack(target:hero);
-            }
+                }
+
+        
+                if (isValidChoice)
+                {
+                    enemy.Attack(target:hero);
+                }
+
+                if (!hero.IsAlive && enemy.IsAlive)
+                {
+                    Console.WriteLine($"{hero.CharacterName} dies at the hand of {enemy.CharacterName}");
+                }
             
-        }
+            }
 
+            if (!hero.IsAlive)
+            {
+                break;
+            }
+      
+        }
+        
         if (hero.IsAlive)
         {
             Console.WriteLine($"{hero.CharacterName} wins");
-        }
-        else if (enemy.IsAlive)
+        }else
         {
-            Console.WriteLine($"{enemy.CharacterName} wins, You lose");
+            Console.WriteLine("You lose");
         }
-        else
-        {
-            Console.WriteLine("Draw");
-        }
-
     }
 }
